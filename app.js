5a7001d407c2fe328f7a391c1617a27c0d44f80e
njp5577 <njp5577@psu.edu>
2022-10-21 08:31:09 -0400

    changing index to homepage

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var homePageRouter = require('./routes/homePage');
 var usersRouter = require('./routes/users');
 var studyGuideRouter = require('./routes/studyGuide');
 var postQuestionRouter = require('./routes/postQuestion');
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', homePageRouter);
 app.use('/users', usersRouter);
 app.use('/studyGuide', studyGuideRouter);
 app.use('/postQuestion', postQuestionRouter);
@@ -44,7 +44,7 @@ app.use(function(err, req, res, next) {
 
 app.get("/", async function(req,res){
 
-  res.render("index")
+  res.render("homePage")
 
 })
 
--- routes/homePage.js
+++ routes/homePage.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Project' });
+  res.render('homePage', { title: 'Project' });
 });
 
 module.exports = router;
