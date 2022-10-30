bdfff4ae1cc897a1d287f0767e9dd244a855bf2d
Ethan <exb5429@psu.edu>
2022-10-30 17:59:05 -0400

    Initial Login and Signup works

--- app.js
+++ app.js
@@ -4,8 +4,70 @@ var morgan = require('Morgan');
 
 var app = express();
 
+// NEW
+var Auth_passport = require('passport');
+const bodyParser = require("body-parser");
+var LocalStrategy = require('passport-local').Strategy;
+var mongoose = require("mongoose");
+const http = require("http");
+
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(Auth_passport.initialize());
+
+
+
+
 app.set("views", path.resolve(__dirname, "views"));
-app.set("view engine", "ejs");
+app.set("view engine", "ejs")
+
+
+
+
+
+var LoginSchema = mongoose.Schema({
+    username :String,
+    password :String
+});
+
+LoginSchema.methods.getUsername = function () {
+    return this.username;
+}
+
+LoginSchema.methods.getPassword = function () {
+    return this.password;
+}
+
+var LoginModel = mongoose.model("logins",LoginSchema);
+
+var currentUser;
+var currentPassword;
+var loggedIn = false;
+
+mongoose.connect("mongodb+srv://test:test@cluster0.flru2.mongodb.net/?retryWrites=true&w=majority");
+
+Auth_passport.use('local', new LocalStrategy(
+    async function (checkUsername, checkPassword, done) {
+        try {
+            console.log("Author Info to Display: " + checkUsername);
+            let User = await LoginModel.findOne({username: checkUsername,password: checkPassword});
+
+            if (User == undefined || User == null) {
+                console.log("Incorrect username or password");
+                return done(null, false, {message: 'Incorrect username or password.'});
+            }
+            else {
+                console.log("User: " + User.username + " has logged in");
+                loggedIn = true;
+                currentUser = User.username;
+                currentPassword = User.password;
+                return done(null, User);
+            }
+        } catch (e) {
+            console.log(e)
+        }
+    }
+));
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, "/public")));
@@ -25,6 +87,24 @@ app.get("/postQuestion", function (req, response){
 app.get("/login", function (req, response){
     response.render("login");
 });
+
+app.post("/login",Auth_passport.authenticate('local',{
+        successRedirect: '/account',
+        failureRedirect: '/login',
+        session: false
+    })
+);
+
+app.post("/signin", async function(req, res){
+    var newUser = req.body.newusername;
+    var newPassword = req.body.password;
+    let New = await LoginModel({username:newUser,password:newPassword});
+    New.save(function (err) {
+        if (err) return console.error(err);
+        console.log(newUser + " saved to logins collection.");
+    });
+    res.render("login");
+})
 
 app.get("*", function (req, response){
     response.render("error");
