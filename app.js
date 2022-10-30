7d2a971d4dceeaa5aeabfaf8eeedd9ed1fc449a3
Ethan <exb5429@psu.edu>
2022-10-30 16:58:30 -0400

    fixed backend to not be bad

--- app.js
+++ app.js
@@ -1,80 +1,35 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var homePageRouter = require('./routes/homePage');
-var usersRouter = require('./routes/users');
-var studyGuideRouter = require('./routes/studyGuide');
-var postQuestionRouter = require('./routes/postQuestion');
-var loginRouter = require('./routes/login');
+var morgan = require('Morgan');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.resolve(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, "/public")));
 
-app.use('/', homePageRouter);
-app.use('/users', usersRouter);
-app.use('/studyGuide', studyGuideRouter);
-app.use('/postQuestion', postQuestionRouter);
-app.use('/login', loginRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.get("/", function (req, response){
+    response.render("homePage");
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.get("/studyGuide", function (req, response){
+    response.render("studyGuide");
 });
 
-app.get("/", async function(req,res){
-
-  res.render("homePage")
-
-})
-
-app.get("/studyGuide", async function(req,res){
-
-  res.render("studyGuide")
-
-})
-
-app.get("/postQuestion", async function(req,res){
-
-  res.render("postQuestion")
-
-})
-
-app.get("/login", async function(req,res){
-
-  res.render("login")
-
-})
-
-app.get('*',function(req,response){
-  response.status(404).render("error");
-})
-
-
-app.listen(3000,function (req,resp) {
-  console.log("Server Running");
+app.get("/postQuestion", function (req, response){
+    response.render("postQuestion");
 });
 
+app.get("/login", function (req, response){
+    response.render("login");
+});
 
+app.get("*", function (req, response){
+    response.render("error");
+});
+
+app.listen(3000, function(req,resp){
+    console.log("Server Running");
+});
--- routes/postQuestion.js
+++
@@ -1,9 +0,0 @@
-var express = require('Express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('postQuestion', { title: 'Project' });
-});
-
-module.exports = router;
